cb087826bcb3cf74e0b4cb3b22b9a232e7124e31
Brandur <brandur@mutelight.org>
2016-12-27 23:05:59 -0700

    Bump Go Spotify SDK

--- vendor/github.com/zmb3/spotify/playlist.go
+++ vendor/github.com/zmb3/spotify/playlist.go
@@ -1,17 +1,3 @@
-// Copyright 2014, 2015 Zac Bergquist
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package spotify
 
 import (
@@ -37,8 +23,8 @@ type PlaylistTracks struct {
 type SimplePlaylist struct {
 	// Indicates whether the playlist owner allows others to modify the playlist.
 	// Note: only non-collaborative playlists are currently returned by Spotify's Web API.
-	Collaborative bool        `json:"collaborative"`
-	ExternalURLs  ExternalURL `json:"external_urls"`
+	Collaborative bool              `json:"collaborative"`
+	ExternalURLs  map[string]string `json:"external_urls"`
 	// A link to the Web API endpoint providing full details of the playlist.
 	Endpoint string `json:"href"`
 	ID       ID     `json:"id"`
@@ -648,7 +634,7 @@ type PlaylistReorderOptions struct {
 // public playlist requires ScopePlaylistModifyPublic.  Reordering tracks in
 // the user's private playlists (including collaborative playlists) requires
 // ScopePlaylistModifyPrivate.
-func (c *Client) ReorderPlaylistTracks(userID, playlistID ID, opt PlaylistReorderOptions) (snapshotID string, err error) {
+func (c *Client) ReorderPlaylistTracks(userID string, playlistID ID, opt PlaylistReorderOptions) (snapshotID string, err error) {
 	spotifyURL := fmt.Sprintf("%susers/%s/playlists/%s/tracks", baseAddress, userID, playlistID)
 	j, err := json.Marshal(opt)
 	if err != nil {

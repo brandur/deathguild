ff6625e1c2b936d2911735f3370d1288a7e32540
Brandur <brandur@mutelight.org>
2017-01-05 20:23:28 -0700

    Add a footer in

--- deathguild.go
+++ deathguild.go
@@ -14,7 +14,7 @@ const (
 	// Release allows CSS and JS assets to be invalidated quickly by changing
 	// their URL. Bump this number whenever something significant changes that
 	// should be invalidated as quickly as possible.
-	Release = "2"
+	Release = "3"
 )
 
 // Playlist is a playlist for a single night of Deathguild.
